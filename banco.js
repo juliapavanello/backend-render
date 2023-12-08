e00d989ab3e7bef456d9aad821d7933e4a5f396a
juliapavanello <julia.pavanello21@gmail.com>
2023-12-08 09:19:27 -0300

    Segundo commit

--- banco.js
+++ banco.js
@@ -2,13 +2,7 @@ const sequelize = require("sequelize");
 require('dotenv').config()
 
 const conexao = new sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect:"mysql",
-        host:process.env.DB_HOST
-    }
+   process.env.DB_HOST,{dialect: "postgres"}
 )
 
 module.exports = { conexao }
